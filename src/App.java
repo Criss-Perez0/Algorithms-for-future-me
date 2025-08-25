9e9c7d388a0c5be777ec4501863562ae19afcbd4
Criss Perez <cpjp2424@gmail.com>
2025-08-24 22:44:34 -0500

    added

--- src/App.java
+++ src/App.java
@@ -2,20 +2,20 @@ import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] a = new int[]{49,27,32,18,1,11,22,44};
-        System.out.println(Arrays.toString(bubblesort(a)));
+        int[] a = new int[] { 49, 27, 32, 18, 1, 11, 22, 44 };
+        System.out.println(Arrays.toString(selectionsort(a)));
     }
 
-     public static int[]  bubblesort(int[] a) {
+    public static int[] bubblesort(int[] a) {
         boolean check = true;
-        while(check){
-            check=false;
-            for(int i=0; i<a.length-1; i++){
-                if(a[i]>a[i+1]){
+        while (check) {
+            check = false;
+            for (int i = 0; i < a.length - 1; i++) {
+                if (a[i] > a[i + 1]) {
                     int temp = a[i];
-                    a[i]=a[i+1];
-                    a[i+1]=temp;
-                    check=true;
+                    a[i] = a[i + 1];
+                    a[i + 1] = temp;
+                    check = true;
                 }
             }
 
@@ -23,8 +23,28 @@ public class App {
         return a;
     }
 
-    public static int[] selectionsort(int[] a){
+    public static int[] selectionsort(int[] a) {
 
+        for (int i = 0; i < a.length; i++) {
+            
+            int index = i;
+                for (int j = i + 1; j < a.length; j++) {
+                    if (a[j] < a[index]) {
+                        index = j;
+                    }
+                }
+                int temp = a[i];
+                a[i] =a[index];
+                a[index]=temp;
+        }
         return a;
     }
+
+    public static int[] insertionsort(int[] a){
+        
+        return a;
+
+        
+    }
+
 }
